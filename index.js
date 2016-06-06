b9106832e281f2f1920cdb936ef9111f0d339655
wodimudi <di.mu@wodi01.com>
2016-06-06 16:39:33 +0800

    v1.0.0 支持抓取美团，饿了么订单并发送邮件给指定人

---
+++ index.js
@@ -0,0 +1,27 @@
+const meituan = require('./lib/meituan');
+const eleme = require('./lib/eleme');
+const mail = require('./lib/mail');
+const logger = require('./lib/logger');
+const promise = require('bluebird');
+const moment = require('moment');
+const config = require('config');
+const _ = require('underscore');
+const accounts = config.get('account');
+let tasks = [];
+let beforeDays = 0;
+_.each(accounts, function (account) {
+    switch (account.type) {
+        case 'meituan':
+            tasks.push(meituan.run(account,beforeDays));
+            break;
+        case 'eleme':
+            //tasks.push(eleme.run(account,beforeDays));
+            break;
+    }
+});
+promise.all(tasks).then(function (files) {
+    //mail.sendMail(beforeDays,files);
+}).catch(function (err) {
+    logger.error(err);
+});
+
