6e3a89ce16465ce57048fbbc08422a5be21a397f
wodimudi <di.mu@wodi01.com>
2016-06-07 13:38:38 +0800

    订单抓取

--- index.js
+++ index.js
@@ -7,21 +7,24 @@ const moment = require('moment');
 const config = require('config');
 const _ = require('underscore');
 const accounts = config.get('account');
-let tasks = [];
-let beforeDays = -1;
-_.each(accounts, function (account) {
-    switch (account.type) {
-        case 'meituan':
-            tasks.push(meituan.run(account,beforeDays));
-            break;
-        case 'eleme':
-            tasks.push(eleme.run(account,beforeDays));
-            break;
-    }
-});
-promise.all(tasks).then(function (files) {
-    mail.sendMail(beforeDays,files);
-}).catch(function (err) {
-    logger.error(err);
-});
+const later = require('later');
+function fetchTask(){
+    let tasks = [];
+    let beforeDays = 1;
+    _.map(accounts, function (account) {
+        switch (account.type) {
+            case 'meituan':
+                tasks.push(meituan.run(account, beforeDays));
+                break;
+            case 'eleme':
+                tasks.push(eleme.run(account, beforeDays));
+                break;
+        }
+    });
+    promise.all(tasks).then(function (files) {
+        mail.sendMail(beforeDays, files);
+    }).catch(function (err) {
+        logger.error(err);
+    });
+}
 
